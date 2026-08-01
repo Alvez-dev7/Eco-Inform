df12c37fca48cda1f6c289afbadf8bdcbd8285ea
João Vitor Alves do Nascimento <freejoao0212@gmail.com>
2026-08-01 03:29:26 -0300

    adicionando interatividade na hero-section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
-const caixa_escura = document.querySelector('#efeito-escuro')
-const hero = document.querySelector('#hero')
+const heroSection = document.querySelector('#hero');
+const caixa_escura = document.querySelector('#efeito-escuro');
 
-    function moverLanterna(c){
-        if (window.innerWidth > 768){
-            const x = c.clientX;
-            const y = c.clientY;
+function moverLanterna(c) {
+    if (window.innerWidth > 768) {
+        
+       
+        const retangulo = heroSection.getBoundingClientRect();
 
-            caixa_escura.style.setProperty('--x', x + 'px');
-            caixa_escura.style.setProperty('--y', y + 'px');
-        }
+        const x = c.clientX - retangulo.left;
+        const y = c.clientY - retangulo.top;
+
+        caixa_escura.style.setProperty('--x', x + 'px');
+        caixa_escura.style.setProperty('--y', y + 'px');
     }
+}
 
-    hero.addEventListener('mousemove', moverLanterna); 
+document.addEventListener("DOMContentLoaded", function(){
 
+    const cards = window.document.querySelectorAll('.card-frase')
+
+    const observador = new IntersectionObserver(function(entries){
+        entries.forEach(function(entry){
+            if(entry.isIntersecting){
+                entry.target.classList.add('visivel')
+            }
+        })
+        
+     },{threshold: 0.2})
+
+    cards.forEach(function(card){
+        observador.observe(card)
+    })
+})
+
+heroSection.addEventListener('mousemove', moverLanterna);
